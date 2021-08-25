6d1babe1013b3c85c5db7e2fe95c84a6235c9cbb
Tendai  Bandawa <tonderaibandawa@gmail.com>
2021-08-25 12:50:42 +0200

    setup data di

--- data/src/main/java/openweather/data/di/DataModule.kt
+++ data/src/main/java/openweather/data/di/DataModule.kt
@@ -1,2 +1,56 @@
 package openweather.data.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import openweather.data.BuildConfig
+import openweather.data.remote.api.OpenWeatherApi
+import openweather.data.repository.OpenWeatherRepositoryImpl
+import openweather.domain.repository.OpenWeatherRepository
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    private val okHttpClient: OkHttpClient by lazy {
+        val interceptor = HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG)
+                HttpLoggingInterceptor.Level.BODY else
+                HttpLoggingInterceptor.Level.NONE
+        }
+        OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(1, TimeUnit.MINUTES)
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    private val baseUrl: String by lazy {
+        BuildConfig.OPEN_WEATHER_BASE_URL
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenWeatherApi(): OpenWeatherApi = retrofit.create(OpenWeatherApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideOpenWeatherRepository(
+        openWeatherApi: OpenWeatherApi) : OpenWeatherRepository = OpenWeatherRepositoryImpl(openWeatherApi)
+
+}
