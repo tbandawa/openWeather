bd9a497b759152fd971214a238b35e7477080cfd
Tendai  Bandawa <tonderaibandawa@gmail.com>
2021-08-26 19:29:11 +0200

    read current weather from respurces

--- data/src/test/java/openweather/data/OpenWeatherRepositoryTest.kt
+++ data/src/test/java/openweather/data/OpenWeatherRepositoryTest.kt
@@ -1,12 +1,42 @@
 package openweather.data
 
-import kotlinx.coroutines.flow.collect
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.runBlocking
-import openweather.domain.models.NetworkResult
+import openweather.data.remote.api.OpenWeatherApi
+import openweather.data.remote.response.*
+import openweather.data.repository.OpenWeatherRepositoryImpl
+import openweather.domain.repository.OpenWeatherRepository
+import org.mockito.MockitoAnnotations
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
 
 class OpenWeatherRepositoryTest {
 
+    @Mock
+    lateinit var openWeatherApi: OpenWeatherApi
 
+    lateinit var openWeatherRepository: OpenWeatherRepository
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        openWeatherRepository = OpenWeatherRepositoryImpl(openWeatherApi)
+    }
+
+    @Test
+    fun `test get current weather`() = runBlocking {
+
+        val response = readCurrentWeather()
+        println(response.toString())
+
+    }
+
+    private fun readCurrentWeather(): CurrentWeatherResponse {
+        val fileContent =
+            this::class.java.classLoader.getResource("current_weather.json").readText()
+        return Gson().fromJson(fileContent, object : TypeToken<CurrentWeatherResponse>() {}.type)
+    }
 
 }
