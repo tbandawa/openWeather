0b94bc0031a14eec2300f2fb744627eb695b0204
Tendai  Bandawa <tonderaibandawa@gmail.com>
2021-08-30 21:20:22 +0200

    create hourly item

--- app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
+++ app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
@@ -3,14 +3,17 @@ package me.tbandawa.android.openweather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,7 +43,10 @@ fun ToolBar() {
         title = {
             Column() {
                 Text(text = "AppBar")
-                Text(text = "AppBar", style = TextStyle(fontSize = 12.sp))
+                Text(
+                    text = "AppBar",
+                    style = TextStyle(fontSize = 12.sp)
+                )
             }
         },
         backgroundColor = Color.White,
@@ -88,10 +94,43 @@ fun MainContent() {
     }
 }
 
+@Composable
+fun HourlyItem() {
+    Column(modifier = Modifier
+        .width(65.dp)
+        .padding(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "8 am",
+            style = TextStyle(
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+        )
+        Image(
+            painter = painterResource(R.drawable.ic_cloud),
+            contentDescription = null,
+            modifier = Modifier
+                .size(35.dp, 35.dp)
+                .padding(0.dp, 5.dp, 0.dp, 5.dp)
+        )
+        Text(
+            text = "22°C",
+            style = TextStyle(
+                color = Color.Black,
+                fontWeight = FontWeight.Light,
+                fontSize = 14.sp
+            )
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     OpenWeatherTheme {
-        MainContent()
+        HourlyItem()
     }
 }
