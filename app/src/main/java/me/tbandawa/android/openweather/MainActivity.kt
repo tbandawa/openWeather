bdd1ae2b2e039fbda6a7a23c29f929ccbd018160
Tendai  Bandawa <tonderaibandawa@gmail.com>
2021-09-01 01:24:04 +0200

    make next 7 days clickable

--- app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
+++ app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -161,6 +162,9 @@ fun BottomRecycler() {
                     top.linkTo(parent.top)
                 }
                 .padding(0.dp, 8.dp, 8.dp, 8.dp)
+                .clickable {
+
+                }
         )
         LazyRow(
             modifier = Modifier
