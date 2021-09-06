ba52699efa854173de1ea0f70ff4ada4ed3868ff
Tendai  Bandawa <tonderaibandawa@gmail.com>
2021-09-06 12:09:15 +0200

    inject pref helper into settings actvity

--- app/src/main/java/me/tbandawa/android/openweather/SettingsActivity.kt
+++ app/src/main/java/me/tbandawa/android/openweather/SettingsActivity.kt
@@ -7,12 +7,19 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import me.tbandawa.android.openweather.ui.components.SettingsContent
 import me.tbandawa.android.openweather.ui.components.SettingsToolBar
 import me.tbandawa.android.openweather.ui.theme.OpenWeatherTheme
+import openweather.data.local.PreferenceHelper
+import javax.inject.Inject
 
 @ExperimentalAnimationApi
+@AndroidEntryPoint
 class SettingsActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var preferenceHelper: PreferenceHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
