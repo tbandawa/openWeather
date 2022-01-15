9eb9e4379ab4867330bedc07e84a40b46b5c3189
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-15 09:29:53 +0200

    make chip stateless

--- app/src/main/java/me/tbandawa/android/openweather/ui/components/UnitChip.kt
+++ app/src/main/java/me/tbandawa/android/openweather/ui/components/UnitChip.kt
@@ -7,43 +7,38 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import me.tbandawa.android.openweather.SettingsViewModel
 import openweather.data.local.PreferenceUnits
-import openweather.data.local.Units
 
 @ExperimentalMaterialApi
 @Composable
 fun UnitChip(
     preferenceUnits: PreferenceUnits,
-    viewModel: SettingsViewModel,
+    setPreference: (PreferenceUnits) -> Unit,
     text: String
 ) {
 
-    val unitObject = remember { mutableStateOf(preferenceUnits) }
+    val unit = remember { mutableStateOf(preferenceUnits) }
 
     Surface(
         color = when (text) {
-            unitObject.value.temperature -> MaterialTheme.colors.onSurface
+            unit.value.temperature -> MaterialTheme.colors.onSurface
             else -> Color.Transparent
         },
         contentColor = when (text) {
-            unitObject.value.temperature -> MaterialTheme.colors.onPrimary
+            unit.value.temperature -> MaterialTheme.colors.onPrimary
             else -> Color.White
         },
         shape = CircleShape,
         modifier = Modifier.padding(3.dp),
         onClick = {
-            var units = viewModel.getUnits()
-            units.temperature = text
-            viewModel.setUnits(units)
-            unitObject.value = units
+            unit.value.temperature = text
+            setPreference(unit.value)
         }
     ) {
         Text(
@@ -52,6 +47,5 @@ fun UnitChip(
             style = MaterialTheme.typography.body2,
             modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp)
         )
-
     }
 }
