4b1278a8504a8aaa90abaa6279adde010f35c9ce
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-16 12:35:53 +0200

    convert speed

--- app/src/main/java/me/tbandawa/android/openweather/extensions/UnitsExtensions.kt
+++ app/src/main/java/me/tbandawa/android/openweather/extensions/UnitsExtensions.kt
@@ -6,18 +6,29 @@ fun Double.toTemperature(unit: String) : String {
     return when (unit) {
         "°C" -> {
             var celcius = this - 273.15
-            "${celcius.roundToInt()}°C"
+            "${celcius.roundToInt()} °C"
         }
         "°F" -> {
             var fahrenheit = (9/5*(this - 273)) + 32
-            "${fahrenheit.roundToInt()}°F"
+            "${fahrenheit.roundToInt()} °F"
         }
         else -> {""}
     }
 }
 
 fun Double.toSpeed(unit: String) : String {
-    return "toSpeed"
+    return when (unit) {
+        "m/s" -> "${this.roundToInt()} m/s"
+        "km/h" -> {
+            var speed = this*3.6
+            "${speed.roundToInt()} km/h"
+        }
+        "mph" -> {
+            var speed = this*2.23694
+            "${speed.roundToInt()} mph"
+        }
+        else -> {""}
+    }
 }
 
 fun Int.toDirection() : String = "${this}°"
@@ -30,20 +41,20 @@ fun Double.toDewPoint(unit: String) : String {
     return when (unit) {
         "°C" -> {
             var celcius = this - 273.15
-            "${celcius.roundToInt()}°C"
+            "${celcius.roundToInt()} °C"
         }
         "°F" -> {
             var fahrenheit = (9/5*(this - 273)) + 32
-            "${fahrenheit.roundToInt()}°F"
+            "${fahrenheit.roundToInt()} °F"
         }
         else -> {""}
     }
 }
 
-fun Int.toVisibility() : String = "${this}m"
+fun Int.toVisibility() : String = "${this} m"
 
-fun Int.toCloudCover() : String = "$this%"
+fun Int.toCloudCover() : String = "$this %"
 
-fun Int.toHumidity() : String = "$this%"
+fun Int.toHumidity() : String = "$this %"
 
 fun Double.toUV() : String = "${this.toInt()}"
