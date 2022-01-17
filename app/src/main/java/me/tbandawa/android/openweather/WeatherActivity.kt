3b1bfa10d4684075d9473b1ecb9181417ca895d7
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-17 19:37:52 +0200

    create weather activity

---
+++ app/src/main/java/me/tbandawa/android/openweather/WeatherActivity.kt
@@ -0,0 +1,60 @@
+package me.tbandawa.android.openweather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.*
+import androidx.compose.runtime.livedata.observeAsState
+import dagger.hilt.android.AndroidEntryPoint
+import me.tbandawa.android.openweather.ui.components.MainContent
+import me.tbandawa.android.openweather.ui.components.MainToolBar
+import me.tbandawa.android.openweather.ui.theme.OpenWeatherTheme
+import openweather.data.local.PreferenceHelper
+import openweather.domain.models.NetworkResult
+import timber.log.Timber
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class WeatherActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var preferenceHelper: PreferenceHelper
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    @ExperimentalAnimationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            Timber.d("${preferenceHelper.observeAsState(preferenceHelper.get()).value}")
+            val preferenceUnits = preferenceHelper.observeAsState(preferenceHelper.get())
+
+            val result = viewModel.oneCallWeather.value
+
+            OpenWeatherTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    when(result) {
+                        is NetworkResult.Loading -> {
+                            LoadingContent()
+                        }
+                        is NetworkResult.Success -> {
+                            Scaffold(
+                                topBar = { MainToolBar() }
+                            ) {
+                                MainContent(result.data!!, preferenceUnits.value)
+                            }
+                        }
+                        is NetworkResult.Error -> {
+                            Timber.d(result.message)
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+}
