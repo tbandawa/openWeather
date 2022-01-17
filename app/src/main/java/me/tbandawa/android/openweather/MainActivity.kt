8b7729346f563841fb9568e85aba94e1eb8e2ddb
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-17 19:54:38 +0200

    ask for permission in main activity

--- app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
+++ app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
@@ -3,40 +3,81 @@ package me.tbandawa.android.openweather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import me.tbandawa.android.openweather.ui.components.MainContent
-import me.tbandawa.android.openweather.ui.components.MainToolBar
+import androidx.compose.runtime.*
+import com.google.accompanist.permissions.rememberPermissionState
 import me.tbandawa.android.openweather.ui.theme.OpenWeatherTheme
-import openweather.domain.models.NetworkResult
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import me.tbandawa.android.openweather.ui.components.PermissionContent
+import me.tbandawa.android.openweather.ui.components.RationaleContent
 import timber.log.Timber
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
 
-    @ExperimentalAnimationApi
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OpenWeatherTheme {
                 Surface(color = MaterialTheme.colors.background) {
-
-
+                    RequireLocationPermission()
                 }
             }
         }
     }
 
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@ExperimentalAnimationApi
+@Composable
+private fun RequireLocationPermission() {
+    // Track if the user doesn't want to see the rationale any more.
+    var doNotShowRationale by rememberSaveable { mutableStateOf(false) }
+
+    // Location permission state
+    val locationPermissionState = rememberPermissionState(
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    when {
+        // If the location permission is granted, then continue to fetch location
+        locationPermissionState.hasPermission -> {
+            Timber.d("location granted")
+        }
+
+        // If the user denied the permission but a rationale should be shown, or the user sees
+        // the permission for the first time, explain why the feature is needed by the app and allow
+        // the user to be presented with the permission again or to not see the rationale any more.
+        locationPermissionState.shouldShowRationale ||
+                !locationPermissionState.permissionRequested -> {
+            if (doNotShowRationale) {
+                Timber.d("Feature not available")
+            } else {
+                PermissionContent { locationPermissionState.launchPermissionRequest() }
+            }
+        }
+
+        // If the criteria above hasn't been met, the user denied the permission. Let's present
+        // the user with a FAQ in case they want to know more and send them to the Settings screen
+        // to enable it the future there if they want to.
+        else -> {
+            RationaleContent()
+        }
+    }
+}
+
+@ExperimentalPermissionsApi
+@Preview(showBackground = true)
+@Composable
+fun MainPreview() {
+    OpenWeatherTheme {
+        RationaleContent()
+    }
 }
