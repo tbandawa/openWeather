a3b742968ab1fbb86e4eaa6143bc69b5602ae10d
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-17 21:29:57 +0200

    launch weather activity when permission granted

--- app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
+++ app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
@@ -16,61 +16,60 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import me.tbandawa.android.openweather.ui.components.PermissionContent
 import me.tbandawa.android.openweather.ui.components.RationaleContent
 import timber.log.Timber
+import android.content.Intent
+import androidx.compose.ui.platform.LocalContext
 
+@OptIn(ExperimentalPermissionsApi::class)
 @ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OpenWeatherTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    RequireLocationPermission()
+                    val context = LocalContext.current
+
+                    // Track if the user doesn't want to see the rationale any more.
+                    var doNotShowRationale by rememberSaveable { mutableStateOf(false) }
+
+                    // Location permission state
+                    val locationPermissionState = rememberPermissionState(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+
+                    when {
+                        // If the location permission is granted, then continue to fetch location
+                        locationPermissionState.hasPermission -> {
+                            val intent = Intent(context.applicationContext, WeatherActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            context.startActivity(intent)
+                        }
+
+                        // If the user denied the permission but a rationale should be shown, or the user sees
+                        // the permission for the first time, explain why the feature is needed by the app and allow
+                        // the user to be presented with the permission again or to not see the rationale any more.
+                        locationPermissionState.shouldShowRationale ||
+                                !locationPermissionState.permissionRequested -> {
+                            if (doNotShowRationale) {
+                                Timber.d("Feature not available")
+                            } else {
+                                PermissionContent { locationPermissionState.launchPermissionRequest() }
+                            }
+                        }
+
+                        // If the criteria above hasn't been met, the user denied the permission. Let's present
+                        // the user with a FAQ in case they want to know more and send them to the Settings screen
+                        // to enable it the future there if they want to.
+                        else -> {
+                            RationaleContent()
+                        }
+                    }
                 }
             }
         }
     }
 
-}
-
-@OptIn(ExperimentalPermissionsApi::class)
-@ExperimentalAnimationApi
-@Composable
-private fun RequireLocationPermission() {
-    // Track if the user doesn't want to see the rationale any more.
-    var doNotShowRationale by rememberSaveable { mutableStateOf(false) }
-
-    // Location permission state
-    val locationPermissionState = rememberPermissionState(
-        android.Manifest.permission.ACCESS_FINE_LOCATION
-    )
-
-    when {
-        // If the location permission is granted, then continue to fetch location
-        locationPermissionState.hasPermission -> {
-            Timber.d("location granted")
-        }
-
-        // If the user denied the permission but a rationale should be shown, or the user sees
-        // the permission for the first time, explain why the feature is needed by the app and allow
-        // the user to be presented with the permission again or to not see the rationale any more.
-        locationPermissionState.shouldShowRationale ||
-                !locationPermissionState.permissionRequested -> {
-            if (doNotShowRationale) {
-                Timber.d("Feature not available")
-            } else {
-                PermissionContent { locationPermissionState.launchPermissionRequest() }
-            }
-        }
-
-        // If the criteria above hasn't been met, the user denied the permission. Let's present
-        // the user with a FAQ in case they want to know more and send them to the Settings screen
-        // to enable it the future there if they want to.
-        else -> {
-            RationaleContent()
-        }
-    }
 }
 
 @ExperimentalPermissionsApi
