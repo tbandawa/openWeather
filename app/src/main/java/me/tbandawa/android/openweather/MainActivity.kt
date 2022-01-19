e2a0528fff7e9b2f02a82a3276e4882ca105f4ea
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-19 04:45:26 +0200

    Location Listener move listener to service refactor main activity

--- app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
+++ app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
@@ -17,30 +17,13 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import me.tbandawa.android.openweather.ui.components.PermissionContent
 import me.tbandawa.android.openweather.ui.components.RationaleContent
 import timber.log.Timber
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import java.lang.Exception
+import me.tbandawa.android.openweather.service.LocationService
 
 @OptIn(ExperimentalPermissionsApi::class)
 @ExperimentalAnimationApi
-class MainActivity : ComponentActivity(), LocationListener {
-
-    var isGPSEnabled = false
-
-    var isNetworkEnabled = false
-
-    var canGetLocation = false
-
-    var location: Location? = null
-
-    var latitude = 0.0
-
-    var longitude = 0.0
-
-    private var locationManager: LocationManager? = null
+class MainActivity : ComponentActivity() {
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,90 +33,18 @@ class MainActivity : ComponentActivity(), LocationListener {
                 Surface(color = MaterialTheme.colors.background) {
                     val context = LocalContext.current
 
-                    // Track if the user doesn't want to see the rationale any more.
                     var doNotShowRationale by rememberSaveable { mutableStateOf(false) }
 
-                    // Location permission state
                     val locationPermissionState = rememberMultiplePermissionsState(
                         listOf(Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.ACCESS_COARSE_LOCATION)
                     )
 
                     when {
-                        // If the location permission is granted, then continue to fetch location
                         locationPermissionState.allPermissionsGranted -> {
-                            Timber.d("location permissions granted")
-
-                            try {
-                                locationManager = context.getSystemService(LOCATION_SERVICE)
-                                        as LocationManager
-
-                                // getting GPS status
-                                isGPSEnabled = locationManager!!
-                                    .isProviderEnabled(LocationManager.GPS_PROVIDER)
-
-                                // getting network status
-                                isNetworkEnabled = locationManager!!
-                                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-                                if (!isGPSEnabled && !isNetworkEnabled) {
-                                    // no network provider is enabled
-                                } else {
-                                    canGetLocation = true
-                                    // First get location from Network Provider
-                                    if (isNetworkEnabled) {
-
-                                        locationManager!!.requestLocationUpdates(
-                                            LocationManager.NETWORK_PROVIDER,
-                                            (1000 * 60 * 1).toLong(),
-                                            10.toFloat(), this
-                                        )
-                                        Timber.d("Network")
-                                        if (locationManager != null) {
-                                            location = locationManager!!
-                                                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-                                            if (location != null) {
-                                                latitude = location!!.latitude
-                                                longitude = location!!.longitude
-                                                Timber.d("location from network: lat -> $latitude, lon -> $longitude")
-                                            }
-                                        }
-                                    } else {
-                                        Timber.d("Network Not Enabled")
-                                    }
-
-                                    // if GPS Enabled get lat/long using GPS Services
-                                    if (isGPSEnabled) {
-                                        if (location == null) {
-                                            locationManager!!.requestLocationUpdates(
-                                                LocationManager.GPS_PROVIDER,
-                                                (1000 * 60 * 1).toLong(),
-                                                10.toFloat(), this
-                                            )
-                                            Timber.d("GPS Enabled")
-                                            if (locationManager != null) {
-                                                location = locationManager!!
-                                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                                                if (location != null) {
-                                                    latitude = location!!.latitude
-                                                    longitude = location!!.longitude
-                                                    Timber.d("location from gps: lat -> $latitude, lon -> $longitude")
-                                                }
-                                            }
-                                        }
-                                    } else {
-                                        Timber.d("GPS Not Enabled")
-                                    }
-                                }
-                            } catch (e: Exception) {
-                                e.printStackTrace()
-                                Timber.d("ERROR -> ${e.message}")
-                            }
-
+                            LocationService(context)
                         }
 
-                        // If the user denied the permission but a rationale should be shown, or the user sees
-                        // the permission for the first time, explain why the feature is needed by the app and allow
-                        // the user to be presented with the permission again or to not see the rationale any more.
                         locationPermissionState.shouldShowRationale ||
                                 !locationPermissionState.permissionRequested -> {
                             if (doNotShowRationale) {
@@ -145,23 +56,15 @@ class MainActivity : ComponentActivity(), LocationListener {
                             }
                         }
 
-                        // If the criteria above hasn't been met, the user denied the permission. Let's present
-                        // the user with a FAQ in case they want to know more and send them to the Settings screen
-                        // to enable it the future there if they want to.
                         else -> {
                             RationaleContent()
                         }
+
                     }
                 }
             }
         }
     }
-
-    override fun onLocationChanged(location: Location) {}
-
-    override fun onProviderEnabled(provider: String) {}
-
-    override fun onProviderDisabled(provider: String) {}
 
 }
 
--- app/src/main/java/me/tbandawa/android/openweather/service/LocationService.kt
+++ app/src/main/java/me/tbandawa/android/openweather/service/LocationService.kt
@@ -4,108 +4,91 @@ import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.location.Location
-import android.location.LocationListener
+import android.location.*
 import android.os.IBinder
-import android.location.LocationManager
 import android.os.Bundle
 import timber.log.Timber
 import java.lang.Exception
 
-class LocationService(val context: Context) : Service(), LocationListener {
+@SuppressLint("MissingPermission")
+class LocationService(
+    val context: Context
+    ) : Service(), LocationListener {
 
-    var isGPSEnabled = false
+    private var location: Location? = null
 
-    var isNetworkEnabled = false
+    private var latitude = 0.0
 
-    var canGetLocation = false
+    private var longitude = 0.0
 
-    var location: Location? = null
+    private var isLocation: Boolean = false
 
-    var latitude = 0.0
+    private var locationManager: LocationManager? = null
 
-    var longitude = 0.0
+    override fun onBind(arg0: Intent?): IBinder? = null
 
-    private val MIN_DISTANCE_CHANGE_FOR_UPDATES: Long = 10
-
-    private val MIN_TIME_BW_UPDATES = (1000 * 60 * 1).toLong()
-
-    protected var locationManager: LocationManager? = null
-
-    init { getGeoLocation() }
-
-    @SuppressLint("MissingPermission")
-    fun getGeoLocation(): Location? {
+    init {
+        locationManager = context.getSystemService(LOCATION_SERVICE) as LocationManager
         try {
-            locationManager = context.getSystemService(LOCATION_SERVICE) as LocationManager
+            locationManager!!.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                (1000 * 60 * 1).toLong(), 10.toFloat(),
+                this
+            )
+            if (locationManager != null) {
+                location = locationManager!!.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                if (location != null) {
+                    latitude = location!!.latitude
+                    longitude = location!!.longitude
 
-            // getting GPS status
-            isGPSEnabled = locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
-
-            // getting network status
-            isNetworkEnabled = locationManager!!
-                .isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-            if (!isGPSEnabled && !isNetworkEnabled) {
-                // no network provider is enabled
-            } else {
-                canGetLocation = true
-                // First get location from Network Provider
-                if (isNetworkEnabled) {
-                    locationManager!!.requestLocationUpdates(
-                        LocationManager.NETWORK_PROVIDER,
-                        MIN_TIME_BW_UPDATES,
-                        MIN_DISTANCE_CHANGE_FOR_UPDATES.toFloat(), this
-                    )
-                    Timber.d("Network")
-                    if (locationManager != null) {
-                        location = locationManager!!
-                            .getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-                        if (location != null) {
-                            latitude = location!!.latitude
-                            longitude = location!!.longitude
-                        }
+                    if (isLocation.not()) {
+                        isLocation = true
+                        Timber.d("location from network: lat -> $latitude, lon -> $longitude")
                     }
-                }
 
-                // if GPS Enabled get lat/long using GPS Services
-                if (isGPSEnabled) {
-                    if (location == null) {
-                        locationManager!!.requestLocationUpdates(
-                            LocationManager.GPS_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES.toFloat(), this
-                        )
-                        Timber.d("GPS Enabled")
-                        if (locationManager != null) {
-                            location = locationManager!!
-                                .getLastKnownLocation(LocationManager.GPS_PROVIDER)
-                            if (location != null) {
-                                latitude = location!!.latitude
-                                longitude = location!!.longitude
-                            }
+                }
+            }
+
+            if (location == null) {
+                locationManager!!.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    (1000 * 60 * 1).toLong(), 10.toFloat(),
+                    this
+                )
+                if (locationManager != null) {
+                    location = locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if (location != null) {
+                        latitude = location!!.latitude
+                        longitude = location!!.longitude
+
+                        if (isLocation.not()) {
+                            isLocation = true
+                            Timber.d("location from gps: lat -> $latitude, lon -> $longitude")
                         }
+
                     }
                 }
             }
         } catch (e: Exception) {
-            e.printStackTrace()
+            Timber.d("ERROR -> ${e.message}")
         }
-        return location
     }
-
 
     fun stopUsingGPS() { locationManager?.removeUpdates(this@LocationService) }
 
-    fun canGetLocation(): Boolean = canGetLocation
+    override fun onLocationChanged(location: Location) {
+        val lat = location.latitude
+        val lng = location.longitude
+        if (isLocation.not()) {
+            isLocation = true
+            Timber.d("on ${location.provider} location changed: lat -> $lat, lon -> $lng")
+        }
+    }
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
 
-    override fun onBind(arg0: Intent?): IBinder? = null
-
-    override fun onLocationChanged(location: Location) {}
+    override fun onProviderDisabled(provider: String) {}
 
     override fun onProviderEnabled(provider: String) {}
-
-    override fun onProviderDisabled(provider: String) {}
 
 }
