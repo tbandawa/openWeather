04d1861059520abb3cbeb8c97b1bab41086f8b95
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-19 11:15:16 +0200

    update Bottom recycler arguments

--- app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
+++ app/src/main/java/me/tbandawa/android/openweather/MainActivity.kt
@@ -1,36 +1,95 @@
 package me.tbandawa.android.openweather
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
 import me.tbandawa.android.openweather.ui.theme.OpenWeatherTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import me.tbandawa.android.openweather.ui.components.PermissionContent
-import me.tbandawa.android.openweather.ui.components.RationaleContent
 import timber.log.Timber
-import androidx.compose.ui.platform.LocalContext
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import me.tbandawa.android.openweather.service.LocationService
+import androidx.constraintlayout.compose.ConstraintLayout
+import dagger.hilt.android.AndroidEntryPoint
+import me.tbandawa.android.openweather.ui.components.*
+import openweather.data.local.PreferenceHelper
+import openweather.domain.models.NetworkResult
+import javax.inject.Inject
 
 @OptIn(ExperimentalPermissionsApi::class)
 @ExperimentalAnimationApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var preferenceHelper: PreferenceHelper
+
+    private val viewModel: WeatherViewModel by viewModels()
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OpenWeatherTheme {
-                Surface(color = MaterialTheme.colors.background) {
+
+                Timber.d("${preferenceHelper.observeAsState(preferenceHelper.get()).value}")
+                val preferenceUnits = preferenceHelper.observeAsState(preferenceHelper.get())
+
+                Timber.d("success_____________________________________________________________")
+
+                /*viewModel.fetchOneCall((-20.1837).toLong(), 28.5203.toLong())
+
+                when(val result = viewModel.oneCallWeather.value) {
+                    is NetworkResult.Loading -> {
+                        LoadingContent()
+                    }
+                    is NetworkResult.Success -> {
+
+                        Scaffold(
+                            topBar = { MainToolBar() }
+                        ) {
+                            ConstraintLayout(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                            ) {
+                                val (topLayout, bottomLayout) = createRefs()
+                                Column(
+                                    modifier = Modifier
+                                        .constrainAs(topLayout) {
+                                            top.linkTo(parent.top)
+                                        }
+                                        .fillMaxWidth()
+                                        .fillMaxHeight()
+                                ) {
+                                    WeatherContent(result.data?.current!!, preferenceUnits.value)
+                                }
+                                Column(
+                                    modifier = Modifier
+                                        .constrainAs(bottomLayout) {
+                                            bottom.linkTo(parent.bottom)
+                                        }
+                                        .fillMaxWidth()
+                                ) {
+                                    BottomRecycler(result.data?.hourly!!, preferenceUnits.value)
+                                }
+                            }
+                        }
+                    }
+                    is NetworkResult.Error -> {
+
+                    }
+                }*/
+
+                /*Surface(color = MaterialTheme.colors.background) {
                     val context = LocalContext.current
 
                     var doNotShowRationale by rememberSaveable { mutableStateOf(false) }
@@ -62,7 +121,7 @@ class MainActivity : ComponentActivity() {
                         }
 
                     }
-                }
+                }*/
             }
         }
     }
