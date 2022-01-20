c21e2791de5f0520096c169595ca2b142c70921b
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-20 21:15:47 +0200

    remove splash from loading content

--- app/src/main/java/me/tbandawa/android/openweather/ui/LoadingContent.kt
+++ app/src/main/java/me/tbandawa/android/openweather/ui/LoadingContent.kt
@@ -1,28 +1,13 @@
 package me.tbandawa.android.openweather.ui
 
 import android.Manifest
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import me.tbandawa.android.openweather.R
 import me.tbandawa.android.openweather.service.Coordinates
 import me.tbandawa.android.openweather.service.LocationService
 
@@ -40,44 +25,6 @@ fun LoadingContent(
         listOf(Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION)
     )
-
-    Surface(color = MaterialTheme.colors.background) {
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            val (centerLayout, textTitle) = createRefs()
-            Image(
-                painter = painterResource(R.drawable.weather),
-                contentDescription = null,
-                modifier = Modifier
-                    .constrainAs(centerLayout) {
-                        start.linkTo(parent.start)
-                        top.linkTo(parent.top)
-                        end.linkTo(parent.end)
-                        bottom.linkTo(parent.bottom)
-                    }
-                    .size(225.dp, 225.dp)
-                    .padding(0.dp, 0.dp, 8.dp, 0.dp)
-            )
-            Text(
-                text = "open Weather",
-                style = TextStyle(
-                    color = Color.Black,
-                    fontWeight = FontWeight.Medium,
-                    fontSize = 12.sp,
-                    textAlign = TextAlign.Center
-                ),
-                modifier = Modifier
-                    .constrainAs(textTitle) {
-                        bottom.linkTo(parent.bottom)
-                    }
-                    .fillMaxWidth()
-                    .height(IntrinsicSize.Max)
-                    .padding(0.dp, 0.dp, 0.dp, 10.dp)
-            )
-        }
-    }
 
     when {
         locationPermissionState.allPermissionsGranted -> {
