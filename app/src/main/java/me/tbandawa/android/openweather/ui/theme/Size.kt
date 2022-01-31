eed9992d8a780ec615b42de3c48ece9e6e5c2542
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-31 21:47:56 +0200

    create sw360dp, sw480dp and large sizes

--- app/src/main/java/me/tbandawa/android/openweather/ui/theme/Size.kt
+++ app/src/main/java/me/tbandawa/android/openweather/ui/theme/Size.kt
@@ -1,6 +1,7 @@
 package me.tbandawa.android.openweather.ui.theme
 
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 class Size (
     weatherIconPadding: Dp,
@@ -10,4 +11,34 @@ class Size (
     hourlyItemWidth: Dp,
     detailItemHeight: Dp,
     detailIconSize: Dp
+)
+
+val sw360dp = Size(
+    weatherIconPadding = 20.dp,
+    weatherIconSize = 100.dp,
+    hourlyIconSize = 40.dp,
+    unitIconSize = 20.dp,
+    hourlyItemWidth = 55.dp,
+    detailItemHeight = 47.dp,
+    detailIconSize = 20.dp
+)
+
+val sw480dp = Size(
+    weatherIconPadding = 25.dp,
+    weatherIconSize = 150.dp,
+    hourlyIconSize = 40.dp,
+    unitIconSize = 25.dp,
+    hourlyItemWidth = 55.dp,
+    detailItemHeight = 50.dp,
+    detailIconSize = 25.dp
+)
+
+val large = Size(
+    weatherIconPadding = 35.dp,
+    weatherIconSize = 200.dp,
+    hourlyIconSize = 55.dp,
+    unitIconSize = 30.dp,
+    hourlyItemWidth = 70.dp,
+    detailItemHeight = 57.dp,
+    detailIconSize = 30.dp
 )
