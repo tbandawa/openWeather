003753f8aa7d0816c14224ae7284679ab6122868
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-01-31 21:22:06 +0200

    create Size class

---
+++ app/src/main/java/me/tbandawa/android/openweather/ui/theme/Size.kt
@@ -0,0 +1,13 @@
+package me.tbandawa.android.openweather.ui.theme
+
+import androidx.compose.ui.unit.Dp
+
+class Size (
+    weatherIconPadding: Dp,
+    weatherIconSize: Dp,
+    hourlyIconSize: Dp,
+    unitIconSize: Dp,
+    hourlyItemWidth: Dp,
+    detailItemHeight: Dp,
+    detailIconSize: Dp
+)
