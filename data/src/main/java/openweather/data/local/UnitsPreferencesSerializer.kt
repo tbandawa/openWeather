e6302195bb1540ca7750f6c82901c8c085458ddd
Tendai  Bandawa <tonderaibandawa@gmail.com>
2022-02-24 21:15:55 +0200

    Proto2 change proto3 to proto2 add fields and default values to UnitsPrefernces message create UnitsPrefencesserializer

--- data/src/main/java/openweather/data/local/UnitsPreferencesSerializer.kt
+++ data/src/main/java/openweather/data/local/UnitsPreferencesSerializer.kt
@@ -2,8 +2,20 @@ package openweather.data.local
 
 import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
-import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
+import com.google.protobuf.InvalidProtocolBufferException
+import openweather.data.UnitsPreferences
 import java.io.InputStream
 import java.io.OutputStream
 
+object UnitsPreferencesSerializer : Serializer<UnitsPreferences> {
+    override val defaultValue: UnitsPreferences = UnitsPreferences.getDefaultInstance()
+    override suspend fun readFrom(input: InputStream): UnitsPreferences {
+        try {
+            return UnitsPreferences.parseFrom(input)
+        } catch (exception: InvalidProtocolBufferException) {
+            throw CorruptionException("Cannot read proto.", exception)
+        }
+    }
 
+    override suspend fun writeTo(t: UnitsPreferences, output: OutputStream) = t.writeTo(output)
+}
