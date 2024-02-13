8e1dfead3d1379e07eca89b7156977a469faa966
Tendai  Bandawa <tonderaibandawa@gmail.com>
2024-02-13 10:56:25 +0200

    move next 7 days link to bottom as button

--- app/src/main/java/me/tbandawa/android/openweather/ui/components/BottomRecycler.kt
+++ app/src/main/java/me/tbandawa/android/openweather/ui/components/BottomRecycler.kt
@@ -1,19 +1,26 @@
 package me.tbandawa.android.openweather.ui.components
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import me.tbandawa.android.openweather.ui.theme.dimensions
 import openweather.domain.models.Hourly
@@ -29,7 +36,7 @@ fun BottomRecycler(
 
     ConstraintLayout {
 
-        val (textHourly, textWeekly, hourlyRow) = createRefs()
+        val (textHourly, hourlyRow, buttonWeekly) = createRefs()
 
         // Hourly text
         Text(
@@ -47,35 +54,14 @@ fun BottomRecycler(
                 .padding(start = 8.dp)
         )
 
-        // Next 7 days forecast link
-        Text(
-            text = "Next 7 Days",
-            style = TextStyle(
-                color = Color.Black,
-                fontWeight = FontWeight.Bold,
-                fontSize = dimensions.forecastTextSize
-            ),
-            textDecoration = TextDecoration.Underline,
-            modifier = Modifier
-                .constrainAs(textWeekly) {
-                    end.linkTo(parent.end)
-                    top.linkTo(parent.top)
-                }
-                .padding(end = 8.dp)
-                .clickable {
-                    navigateToForecast.invoke()
-                }
-        )
-
         // Hourly horizontal list
         LazyRow(
             modifier = Modifier
                 .constrainAs(hourlyRow) {
                     top.linkTo(textHourly.bottom)
-                    top.linkTo(textWeekly.bottom)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
+                    bottom.linkTo(buttonWeekly.top)
                 }
                 .padding(0.dp, 0.dp, 0.dp, 2.dp)
                 .fillMaxWidth()
@@ -89,6 +75,32 @@ fun BottomRecycler(
             }
         }
 
+        // Next 7 days forecast button
+        Surface(
+            color = Color.Black,
+            contentColor = Color.White,
+            shape = CircleShape,
+            modifier = Modifier
+                .constrainAs(buttonWeekly) {
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                }
+                .padding(end = 16.dp),
+            onClick = {
+                navigateToForecast.invoke()
+            }
+        ) {
+            Text(
+                text = "Next 7 Days",
+                textAlign = TextAlign.Center,
+                style = TextStyle(
+                    color = Color.White,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 10.sp
+                ),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 4.dp)
+            )
+        }
     }
-
 }
